940d328a8c7fdb6e02442360c9f26de71ecc6de8
romnick <romnicksanjuan9@gmail.com>
2024-04-15 17:49:07 +0800

    sdsd

--- server/server.js
+++ server/server.js
@@ -1,31 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./routes/userRoute')
+const cors = require('cors')
 const app = express()
+const User = require('./model/user.js')
 
 
 mongoose.connect('mongodb+srv://romnick:1234@romnickdb.e14diyv.mongodb.net/mobile-dev')
-.then(() => console.log('connected to database'))
+    .then(() => console.log('connected to database'))
+
 app.use(express.json())
 // app.use('/', router)
 
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.json('welcome page')
 })
 
-app.get('/register',async (req,res) =>{
-    const {name,username,password} = req.body;
+app.post('/register', async (req, res) => {
+    const { name, username, password } = req.body;
 
     try {
-        const saveUser = new User({name,username,password})
+        const saveUser = new User({ name, username, password })
         await saveUser.save();
-        res.json({message:'registered success'})
-        
+        // res.json({message:'registered success'})
+
     } catch (error) {
         console.log(error)
     }
 })
 
-app.listen(3000, () =>{
+app.listen(3000, () => {
     console.log('server is running')
 })
